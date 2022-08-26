750dd83ebb36c8c26e0f61065c964cbdda772c60
Ilya Shmaust <ilyashmaust1999@gmail.com>
2022-08-26 13:44:53 +0300

    Initial commit

---
+++ src/main/java/user/ishmaust/shophelper/ShopHelperApplication.java
@@ -0,0 +1,13 @@
+package user.ishmaust.shophelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/user/ishmaust/shophelper/ShopHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package user.ishmaust.shophelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
